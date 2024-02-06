b9d576d637da1277f8420daee52ec38c0168f506
Dinesh Chhantyal <myagdichhantyal@gmail.com>
2024-02-06 11:15:21 -0600

    Remove unnecessary line in config.ts

--- utils/config.ts
+++ utils/config.ts
@@ -2,6 +2,5 @@ export default {
   COMPANY_NAME: "CheerPal",
   DISPLAY_URL: "cheerpal.vercel.app/",
   BASE_URL: "https://cheerpal-frontend.vercel.app/",
-
   CONTACT_EMAIL: "myagdichhantyal@gmail.com",
 };

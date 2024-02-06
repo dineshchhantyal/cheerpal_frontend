b80e44e85f34a08963f6490021a04a2452353678
Dinesh Chhantyal <myagdichhantyal@gmail.com>
2024-02-06 10:14:40 -0600

    Update footer text color

--- components/Footer/footer.tsx
+++ components/Footer/footer.tsx
@@ -3,7 +3,7 @@ import React from "react";
 export const Footer = () => {
   return (
     <footer className="flex flex-col gap-2 sm:flex-row py-6 w-full shrink-0 items-center px-4 md:px-6">
-      <p className="text-xs text-gray-200 dark:text-gray-400">
+      <p className="text-xs text-gray-500 dark:text-gray-400">
         © 2024 CheerPal. All rights reserved.
       </p>
       <nav className="sm:ml-auto flex gap-4 sm:gap-6">

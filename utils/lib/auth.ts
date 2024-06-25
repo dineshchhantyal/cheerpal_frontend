1886568f5c51980363c813df5c16cf7c87ebf189
Dinesh Chhantyal <myagdichhantyal@gmail.com>
2024-06-25 12:03:03 -0500

    Update .env file, Docker setup, README.md and dependencies,

--- utils/lib/auth.ts
+++ utils/lib/auth.ts
@@ -3,6 +3,7 @@ import type { NextAuthOptions } from "next-auth";
 import CredentialsProvider from "next-auth/providers/credentials";
 import { prisma } from "./prisma";
 export const authOptions: NextAuthOptions = {
+  secret: process.env.NEXTAUTH_SECRET,
   session: {
     strategy: "jwt",
   },

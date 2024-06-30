1b1c4470332ae747db6f25e48a5513cb502a286c
Dinesh Chhantyal <myagdichhantyal@gmail.com>
2024-06-29 23:15:15 -0500

    chore: Remove unused Google authentication provider import

--- utils/lib/auth.ts
+++ utils/lib/auth.ts
@@ -5,7 +5,6 @@ import CredentialsProvider from "next-auth/providers/credentials";
 import { PrismaAdapter } from "@auth/prisma-adapter";
 import { Adapter } from "next-auth/adapters";
 
-import GoogleProvider from "next-auth/providers/google";
 import prisma from "./prisma";
 import GoogleProvider from "next-auth/providers/google";
 
